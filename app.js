b0970be0d9de88d9d17d9aca9ab7fc4f2481de7a
RicardoJoelVergara <joel27carp@gmail.com>
2024-01-26 13:34:29 -0300

    se agrego el html y css de ingreso y registro

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const app = express();
 const path = require('node:path');
 // Routes
 
-app.get('/', ( req, res ) =>{
-    const pathHome = path.join(__dirname, 'views/home.html')
+//app.get('/', ( req, res ) =>{
+  //  const pathHome = path.join(__dirname, 'views/home.html')
     
-  res.sendFile(pathHome);
+  //res.sendFile(pathHome);
+//} )
+app.get('/', ( req, res ) =>{
+  const pathHome = path.join(__dirname, 'views/home.html')
+  
+res.sendFile(pathHome);
+} )
+app.get('/registro', ( req, res ) =>{
+  const pathRegistro = path.join(__dirname, 'views/registro.html')
+res.sendFile(pathRegistro);
+} )
+app.get('/ingreso', ( req, res ) =>{
+res.sendFile( path.join(__dirname, 'views/ingreso.html'));
 } )
 
  // Public (static) Listen Server
